19c312baea2ed970f60ca26e4faafde176650eff
MadhanKumaryk <madhankumaryk@gmail.com>
2022-02-26 18:05:37 +0530

    commit

--- insurance/src/app/app-routing.module.ts
+++ insurance/src/app/app-routing.module.ts
@@ -2,9 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { HeaderComponent } from './header/header.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 const routes: Routes = [{'path': '', component: HomeComponent},
-{'path': 'header', component: HeaderComponent}];
+{'path': 'header', component: HeaderComponent},
+{'path': 'login', component: LoginComponent},
+{'path': 'register', component: RegisterComponent},];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
